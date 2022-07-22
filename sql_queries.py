83759f566adf7d23587ad76cb7f277778d470497
alexandrabaturina <alexandra.v.baturina@gmail.com>
2022-07-21 20:16:55 -0700

    feat: insert records

--- sql_queries.py
+++ sql_queries.py
@@ -59,22 +59,66 @@ time_table_create = ("""
         weekday int);
 """)
 
-# INSERT RECORDS
+# Insert records
 
 songplay_table_insert = ("""
+    INSERT INTO songplays (
+        start_time,
+        user_id,
+        level,
+        song_id,
+        artist_id,
+        session_id,
+        location,
+        user_agent)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (songplay_id) DO NOTHING
 """)
 
 user_table_insert = ("""
+    INSERT INTO users (
+        user_id,
+        first_name,
+        last_name,
+        gender,
+        level)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id) DO NOTHING
 """)
 
 song_table_insert = ("""
+    INSERT INTO songs (
+        song_id,
+        title,
+        artist_id,
+        year,
+        duration)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (song_id) DO NOTHING
 """)
 
 artist_table_insert = ("""
+    INSERT INTO artists (
+        artist_id,
+        name,
+        location,
+        latitide,
+        longitude)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id) DO NOTHING
 """)
 
-
 time_table_insert = ("""
+    INSERT INTO time (
+        start_time,
+        hour,
+        day,
+        week,
+        month,
+        year,
+        weekday)
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (start_time) DO NOTHING
 """)
 
 # FIND SONGS
@@ -82,7 +126,20 @@ time_table_insert = ("""
 song_select = ("""
 """)
 
-# QUERY LISTS
+# Query lists
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [
+    songplay_table_create,
+    user_table_create,
+    song_table_create,
+    artist_table_create,
+    time_table_create
+]
+
+drop_table_queries = [
+    songplay_table_drop,
+    user_table_drop,
+    song_table_drop,
+    artist_table_drop,
+    time_table_drop
+]
