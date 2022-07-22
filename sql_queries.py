00d2f531960df2b2c62988be4eb4b492fdf35ce2
alexandrabaturina <alexandra.v.baturina@gmail.com>
2022-07-21 20:12:42 -0700

    feat: create tables

--- sql_queries.py
+++ sql_queries.py
@@ -6,21 +6,57 @@ song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
 time_table_drop = "DROP TABLE IF EXISTS time"
 
-# CREATE TABLES
+# Create tables
 
 songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplays (
+        songplay_id serial PRIMARY KEY,
+        start_time timestamp,
+        user_id int NOT NULL,
+        level varchar,
+        song_id char(18) NOT NULL,
+        artist_id char(18) NOT NULL,
+        session_id int,
+        location varchar,
+        user_agent varchar);
 """)
 
 user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id int PRIMARY KEY,
+        first_name varchar,
+        last_name varchar,
+        gender char(1),
+        level varchar);
 """)
 
 song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs (
+        song_id char(18) PRIMARY KEY,
+        title varchar,
+        artist_id char(18),
+        year int,
+        duration float);
 """)
 
 artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists (
+        artist_id char(18) PRIMARY KEY,
+        name varchar,
+        location varchar,
+        latitide float,
+        longitude float);
 """)
 
 time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time (
+        start_time timestamp PRIMARY KEY,
+        hour int,
+        day int,
+        week int,
+        month int,
+        year int,
+        weekday int);
 """)
 
 # INSERT RECORDS
