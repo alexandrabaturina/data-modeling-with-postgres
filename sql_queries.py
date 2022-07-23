b997cd22124b530e3871d211e570643687d8294d
alexandrabaturina <alexandra.v.baturina@gmail.com>
2022-07-22 17:51:02 -0700

    fix: add WHERE filter in the song select query

--- sql_queries.py
+++ sql_queries.py
@@ -127,6 +127,7 @@ time_table_insert = ("""
 song_select = ("""
     SELECT song_id, songs.artist_id
     FROM songs JOIN artists ON songs.artist_id = artists.artist_id
+    WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s
 """)
 
 # Query lists
